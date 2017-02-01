5aea5569927fe24bae6dd132b0554ee91066e0b9
ohrak22gmail <ohrak22@gmail.com>
2017-02-01 17:35:04 +0800

    페북, 구글 추가

--- BasisWebServer/auth.php
+++ BasisWebServer/auth.php
@@ -16,8 +16,11 @@
 	    {
             switch($data_decode["Action"])
             {
-                case "LoginUserWithAuthToken":
-                    LoginUserWithAuthToken($data_decode["DeviceID"]);
+                case "Login":
+                    Login($data_decode["DeviceID"]);
+                    break;
+                case "FacebookLogin":
+                    FacebookLogin($data_decode["DeviceID"], $data_decode["AccessToken"]);
                     break;
                 default: // Action not listed, invalid request
                     //ReturnErrorToUser("E104");
@@ -30,7 +33,7 @@
         }
     }
 
-    function LoginUserWithAuthToken($DeviceID)
+    function Login($DeviceID)
     {
         $servername = "localhost";
         $username = "root";
@@ -54,19 +57,71 @@
                 $result = mysqli_query($conn, "SELECT MAX(id) FROM account");
                 $resultArr = mysqli_fetch_array($result);
                 $next_id = $resultArr['MAX(id)'] + 1;
-                echo "next_id ", $next_id , "\n";
 
-                $query =    "INSERT INTO account (ID, DeviceID, LoginType)
-                            VALUES ('$next_id', '$DeviceID', 'Google')";
+                $query = "INSERT INTO account (ID, DeviceID, LoginType) VALUES ('$next_id', '$DeviceID', 'Google')";
+                $result = mysqli_query($conn, $query);
 
-                if (mysqli_query($conn, $query))
-                {
-                    echo "New record created successfully";
-                }
-                else
-                {
-                    echo "Error: " . $sql . "<br>" . $conn->error;
-                }
+                $query = "SELECT * FROM account WHERE DeviceID = '$DeviceID'";
+                $result = mysqli_query($conn, $query);
+                $row = mysqli_fetch_assoc($result);
+
+                $return_arr = array();
+                array_push($return_arr,$return);
+                echo json_encode($return_arr);
+            }
+            else
+            {
+                $return_arr = array();
+                array_push($return_arr,$row);
+                echo json_encode($return_arr);
+            }
+
+        }
+        else
+        {
+            echo "Result is null\n";
+        }
+
+        mysqli_free_result($result);
+		mysqli_close($conn);
+
+    }
+
+    function FacebookLogin($DeviceID, $AccessToken)
+    {
+        $servername = "localhost";
+        $username = "root";
+        $password = "mm7982";
+        $dbname = "basis";
+
+        $conn = mysqli_connect($servername, $username, $password);
+        if ($conn->connect_error)
+        {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        mysqli_select_db($conn, $dbname);
+        $query = "SELECT * FROM account WHERE DeviceID = '$DeviceID'";
+        $result = mysqli_query($conn, $query);
+        $row = mysqli_fetch_assoc($result);
+
+        if($result != NULL)
+        {
+            if($row == NULL)
+            {
+                $result = mysqli_query($conn, "SELECT MAX(id) FROM account");
+                $resultArr = mysqli_fetch_array($result);
+                $next_id = $resultArr['MAX(id)'] + 1;
+
+                $query = "INSERT INTO account (ID, DeviceID, LoginType) VALUES ('$next_id', '$DeviceID', 'Facebook', '$AccessToken')";
+                $result = mysqli_query($conn, $query);
+
+                $query = "SELECT * FROM account WHERE DeviceID = '$DeviceID'";
+                $result = mysqli_query($conn, $query);
+                $row = mysqli_fetch_assoc($result);
+
+                $return_arr = array();
+                array_push($return_arr,$return);
+                echo json_encode($return_arr);
             }
             else
             {
